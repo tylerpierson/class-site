81b8110944aece6000e01eae9963a5bc590dd0a7
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-07-31 17:43:07 -0500

    First commit

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -6,7 +6,7 @@ function Bio() {
         <div className={styles.contentContainer}>
             <h1 className={styles.title}>Software<br/>Engineer</h1>
         </div>
-        <p className={styles.titleDescription}>My role is as dynamic as the code I craft. I'm not just a coder; I'm an architect, a problem-solver, and an innovator.  </p>
+        <p className={styles.titleDescription}>My role is...  </p>
     </div>
   )
 }
