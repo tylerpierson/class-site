cd56819c1d5c90c91dd13695619588baebbaf84c
Tyler <105254530+tylerpierson@users.noreply.github.com>
2024-08-02 11:36:56 -0500

    Fixed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('./config/database')
 
 const app = require('./app-server')
 
-const PORT = process.env.PORT || 8002
+const PORT = 8003
 
 app.listen(PORT, () => {
 	console.log(`App is listening on ${PORT}.`)
